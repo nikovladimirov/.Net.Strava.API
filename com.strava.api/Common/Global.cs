c341cc2d5aa75a2b26855fe7b979df279a25bce7
RobertEves92 <roberteves92@gmail.com>
2020-09-16 16:55:30 +0100

    load values if they have been saved, or throw an error if no file exists

--- com.strava.api/Common/Global.cs
+++ com.strava.api/Common/Global.cs
@@ -4,6 +4,7 @@ namespace com.strava.api.Common
 {
     public static class Global
     {
+        private static string authfile = System.IO.Directory.GetCurrentDirectory() + "\\auth";
         public static string Code { get; set; }
 
         public static string Token { get; set; }
@@ -16,13 +17,26 @@ namespace com.strava.api.Common
 
         public static void SaveValues()
         {
-            using (IniFile iniFile = new IniFile(System.IO.Directory.GetCurrentDirectory() + "\\auth"))
+            using (IniFile iniFile = new IniFile(authfile))
             {
                 iniFile.IniWriteValue("tokens", "auth", Token);
                 iniFile.IniWriteValue("expiry", "in", ExpiresIn);
                 iniFile.IniWriteValue("expiry", "at", ExpiresAt.ToString());
                 iniFile.IniWriteValue("tokens", "refresh", RefreshToken);
             }
+        }
+
+        public static void LoadValues()
+        {
+            if (System.IO.File.Exists(authfile))
+            {
+                using (IniFile iniFile = new IniFile(authfile))
+                {
+                    Token = iniFile.IniReadValue("tokens", "auth");
+                    ExpiresAt = DateTime.Parse(iniFile.IniReadValue("expiry", "at"));
+                }
+            }
+            else { throw new System.IO.FileNotFoundException($"The requested file {authfile} does not exist"); }
         }
     }
 }
